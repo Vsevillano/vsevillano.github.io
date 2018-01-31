6f7150ae2aa6e64de1ba309e9b2c440f53a5817a
Victoriano Sevillano Vega <victorianosevillano@gmail.com>
2018-01-31 19:14:01 +0100

    Cambio cosillas

---
+++ js/bajar.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('a#sobre-mi').click(function() {
+        $('html, body').animate({
+            scrollTop: $("div.sobre-mi").offset().top
+        }, 1000)
+    }),
+    $('a#proyectos').click(function() {
+        $('html, body').animate({
+            scrollTop: $("div.proyectos").offset().top
+        }, 1000)
+    }),
+    $('button#verMas').click(function() {
+        $('html, body').animate({
+            scrollTop: $("div.sobre-mi").offset().top
+        }, 1000)
+    })
+});
+
