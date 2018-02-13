674de200dcd1c82c2f481ecc7e5d83fdecb28d71
Victoriano Sevillano Vega <victorianosevillano@gmail.com>
2018-02-13 14:00:40 +0100

    Minify JS

--- js/menu.js
+++ js/menu.js
@@ -1,46 +1,5 @@
-$(document).ready(function() {
-    $(document).delegate('.open', 'click', function(event){
-        $(this).addClass('oppenned');
-        event.stopPropagation();
-    })
-
-    $(document).delegate('.open_black', 'click', function(event){
-        $(this).addClass('oppenned');
-        event.stopPropagation();
-    })
-
-    $(document).delegate('body', 'click', function(event) {
-        $('.open').removeClass('oppenned');
-    })
-
-    $(document).delegate('body', 'click', function(event) {
-        $('.open_black').removeClass('oppenned');
-    })
-
-    $(document).delegate('.cls', 'click', function(event){
-        $('.open').removeClass('oppenned');
-        event.stopPropagation();
-    });
-
-    $(document).delegate('.cls', 'click', function(event){
-        $('.open_black').removeClass('oppenned');
-        event.stopPropagation();
-    });
-
-    var scrollTop = 0;
-    $(window).scroll(function(){
-      scrollTop = $(window).scrollTop();
-      
-      if (scrollTop >= 600) {
-        $('#open').addClass('open_black');
-        $('#open').removeClass('open');
-
-      } else if (scrollTop < 600) {
-        $('#open').removeClass('open_black');
-        $('#open').addClass('open');
-
-      } 
-      
-    }); 
-    
-  });
+$(document).ready(function(){$(document).delegate('.open','click',function(event){$(this).addClass('oppenned');event.stopPropagation();})
+$(document).delegate('.open_black','click',function(event){$(this).addClass('oppenned');event.stopPropagation();})
+$(document).delegate('body','click',function(event){$('.open').removeClass('oppenned');})
+$(document).delegate('body','click',function(event){$('.open_black').removeClass('oppenned');})
+$(document).delegate('.cls','click',function(event){$('.open').removeClass('oppenned');event.stopPropagation();});$(document).delegate('.cls','click',function(event){$('.open_black').removeClass('oppenned');event.stopPropagation();});var scrollTop=0;$(window).scroll(function(){scrollTop=$(window).scrollTop();if(scrollTop>=600){$('#open').addClass('open_black');$('#open').removeClass('open');}else if(scrollTop<600){$('#open').removeClass('open_black');$('#open').addClass('open');}});});
